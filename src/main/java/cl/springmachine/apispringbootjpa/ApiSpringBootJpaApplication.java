400e1dbc4a9b354f55a7f6bbaa1759a1d60020d9
Roberto Fuentes <roberfuentesg@gmail.com>
2024-01-05 01:03:34 -0300

    Init project

---
+++ src/main/java/cl/springmachine/apispringbootjpa/ApiSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package cl.springmachine.apispringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/springmachine/apispringbootjpa/ApiSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.springmachine.apispringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
